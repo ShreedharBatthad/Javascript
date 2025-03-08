e662026645790a090957a8375df147a79462edcc
Shreedhar <shreedharbatthad@gmail.com>
2025-03-08 20:12:12 +0530

    commit

---
+++ 4_Loops/demo3.js
@@ -0,0 +1,9 @@
+let sum = 0
+let n = prompt("enter")
+n = Number.parseInt(n)
+for (let i =0; i< n; i++){
+
+  sum += (i +1)
+}
+
+console.log(n + sum)
---
+++ 4_Loops/demo4.js
@@ -0,0 +1,10 @@
+const animal ={
+    Name : 'Deer',
+    Color : "Mixed",
+    Height: '5',
+    Consume: 'Veg'
+}
+
+for (let N in animal){
+    console.log(N + " : " +  animal[N])
+}
